0c147ebdf1afba2c41c8a91dcfea6a9506e7d2f9
SmithSub <smitheide@hotmail.com>
2019-11-06 15:53:24 +0100

    Oppg2

--- src/no/hvl/dat100/jplab12/oppgave2/Tekst.java
+++ src/no/hvl/dat100/jplab12/oppgave2/Tekst.java
@@ -5,35 +5,38 @@ import no.hvl.dat100.jplab12.common.TODO;
 
 public class Tekst extends Innlegg {
 
-	// TODO: objektvariable 
+	private String tekst;
 	
 	public Tekst () {
 		
 	}
 	
 	public Tekst(int id, String bruker, String dato, String tekst) {
-		throw new UnsupportedOperationException(TODO.constructor("Tekst"));
+		super(id,bruker,dato);
+		this.tekst = tekst;
 	}
 	
 	public Tekst(int id, String bruker, String dato, int likes, String tekst) {
-		throw new UnsupportedOperationException(TODO.constructor("Tekst"));
+		super(id,bruker,dato,likes);
+		this.tekst = tekst;
 	}
 	
 	public String getTekst() {
-		throw new UnsupportedOperationException(TODO.method());
+		return tekst;
 
 	}
 
 	public void setTekst(String tekst) {
-		throw new UnsupportedOperationException(TODO.method());
+		this.tekst = tekst;
 	}
 
 	@Override
 	public String toString() {
-		throw new UnsupportedOperationException(TODO.method());
-
+		
+		String str = "TEKST\n" + getId() + "\n" + getBruker() + "\n" + getDato() + "\n" + getLikes() + "\n" + tekst + "\n";
+		return str;
 	}
-	
+	//TEKST\n1\nOle Olsen\n23-10-2019\n0\nen tekst\n
 	// Metoden nedenfor er kun for valgfri oppgave 6
 	public String toHTML() {
 			
